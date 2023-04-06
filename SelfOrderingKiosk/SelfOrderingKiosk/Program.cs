a116f22d9258093858811d500c51f562d5e02c5f
zajdelb <bartlomiej@DESKTOP-6FIAH4L>
2023-04-06 18:48:57 +0200

    Init

---
+++ SelfOrderingKiosk/SelfOrderingKiosk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SelfOrderingKiosk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
