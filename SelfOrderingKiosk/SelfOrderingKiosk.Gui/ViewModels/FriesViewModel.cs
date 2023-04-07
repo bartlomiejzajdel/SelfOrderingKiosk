40849da124b9b641e50d2273025036e3059e3420
zajdelb <bartlomiej@DESKTOP-6FIAH4L>
2023-04-07 19:16:41 +0200

    Rename

--- SelfOrderingKiosk/SelfOrderingKiosk.Gui/ViewModels/FriesViewModel.cs
+++ SelfOrderingKiosk/SelfOrderingKiosk.Gui/ViewModels/FriesViewModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SelfOrderingKiosk.Gui.ViewModel
+namespace SelfOrderingKiosk.Gui.ViewModels
 {
     class FriesViewModel
     {
--- SelfOrderingKiosk/SelfOrderingKiosk.Gui/Views/FriesView.xaml.cs
+++ SelfOrderingKiosk/SelfOrderingKiosk.Gui/Views/FriesView.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SelfOrderingKiosk.Gui.View
+namespace SelfOrderingKiosk.Gui.Views
 {
     /// <summary>
     /// Interaction logic for FriesView.xaml
