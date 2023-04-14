6834f3bf17a6173e2778289a4c24a7d22ffe8210
zajdelb <bartlomiej@DESKTOP-6FIAH4L>
2023-04-14 18:18:31 +0200

    Clean

--- SelfOrderingKiosk/SelfOrderingKiosk/DataAccess/Menu.cs
+++ SelfOrderingKiosk/SelfOrderingKiosk/DataAccess/Menu.cs
@@ -6,10 +6,6 @@ namespace SelfOrderingKiosk.DataAccess
 {
     public class Menu
     {
-        public Menu()
-        {
-            DbConfiguration.LoadConfiguration();
-        }
         public List<Product> BurgersList { get; set; } = ProductsService.GetBurgers();
         public List<Product> FriesList { get; set; } = ProductsService.GetFries();
         public List<Product> DrinksList { get; set; } = ProductsService.GetDrinks();
